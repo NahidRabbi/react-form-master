b94b6d49863b3d9c29c75e6b7e01c73cdd1b871d
nahid <nahidrabbi045@gmail.com>
2024-04-01 02:43:24 +0600

    first commit

---
+++ src/__form__.js
@@ -0,0 +1,11 @@
+/** 
+ * 1. use form tag and onSubmit handler with event (e) from e.target.[name of the input field].value and name to get values,
+ *
+ * 2. controlled element: use individual field state for each input field.
+ * 
+ * 3. controlled element: on object in a state holding all the input fields value.
+ * 
+ * 4. uncontrolled element: useRef to create  a reference to the element and access value by using like: nameref.current.value
+ * 
+ * 
+ */
