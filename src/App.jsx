cc89b51466c0b1dac7e046b7a549818f85410686
nahid <nahidrabbi045@gmail.com>
2024-04-01 07:10:03 +0600

    added a some form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/SimpleForm/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
+  const handleSignUpSubmit = data =>{
+   console.log('sign up data', data);
+}
+
+  const handleUpdateProfile = data =>{
+     console.log('update profile', data);
+  }
 
   return (
     <>
@@ -13,7 +21,19 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={'Sign up'} handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign up</h2>
+          <p>please Sign uo right now</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm formTitle={'Profile update'} handleSubmit={handleUpdateProfile} submitBtnText='update'>
+        <div>
+          <h2>update profile</h2>
+          <p>Always keep your profile update</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
